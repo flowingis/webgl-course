2477ede3f698dad1a76e374cba0012ac6fd7c155
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-11-16 23:31:35 +0100

    random triangles

--- webgl-fundamentals/index.js
+++ webgl-fundamentals/index.js
@@ -8,6 +8,8 @@ import {
   setResolutionToUniform
 } from './utils'
 
+import drawTriangle from './drawTriangle'
+
 const extractDataFromBuffer = (gl, positionAttributePointer) => {
   gl.enableVertexAttribArray(positionAttributePointer)
 
@@ -25,23 +27,6 @@ const main = () => {
     return
   }
 
-  const FIRST_TRIANGLE = [
-    ...[0, 0],
-    ...[Math.floor(window.innerWidth / 2), 0],
-    ...[0, Math.floor(window.innerHeight / 2)]
-  ]
-
-  const SECOND_TRIANGLE = [
-    ...[window.innerWidth, window.innerHeight],
-    ...[Math.floor(window.innerWidth / 2), window.innerHeight],
-    ...[window.innerWidth, Math.floor(window.innerHeight / 2)]
-  ]
-
-  const TRIANGLE_POINTS = new Float32Array([
-    ...FIRST_TRIANGLE,
-    ...SECOND_TRIANGLE
-  ])
-
   // Creating Shaders from source
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
@@ -58,17 +43,10 @@ const main = () => {
   // Bind the buffer it to WebGL ARRAY_BUFFER global
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
-  // Put the data in the buffer
-  gl.bufferData(gl.ARRAY_BUFFER, TRIANGLE_POINTS, gl.STATIC_DRAW)
-
   // Rendering
 
   resizeCanvasToDisplaySize(gl.canvas)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-  // Clear the canvas
-  gl.clearColor(0.066666666666667, 0.501960784313725, 0.67843137254902, 1) // #1280AD
-  gl.clear(gl.COLOR_BUFFER_BIT) // Enable color writing
 
   gl.useProgram(program)
 
@@ -76,7 +54,9 @@ const main = () => {
 
   setResolutionToUniform(gl, program, 'u_resolution')
 
-  gl.drawArrays(gl.TRIANGLES, 0, TRIANGLE_POINTS.length / 2)
+  for (let i = 0; i < 100; ++i) {
+    drawTriangle(gl, program)
+  }
 }
 
 main()
--- webgl-fundamentals/shaders/fragment.glsl
+++ webgl-fundamentals/shaders/fragment.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
 
+uniform vec4 u_color;
+ 
 void main() {
-    gl_FragColor = vec4(0.870588235294118, 0.909803921568627, 0.192156862745098, 1); //#DEE831
+    gl_FragColor = u_color;
 }

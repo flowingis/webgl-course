228f22cec3feda1c9a7be379362da1ed344bf5ab
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-11-22 16:02:02 +0100

    first square

--- pixi.js/index.js
+++ pixi.js/index.js
@@ -1,5 +1,18 @@
-import { Application } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
+
+const createSquare = (size, color) => {
+  const square = new Graphics()
+
+  square.beginFill(color)
+  square.drawRect(0, 0, size, size)
+
+  return square
+}
 
 const app = new Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x009DCD})
+
+const square = createSquare(200, 0xDEE831)
+
+app.stage.addChild(square)
 
 document.body.appendChild(app.view)

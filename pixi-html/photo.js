2d26e209f91250af93cbcaafe6bd36acaccdcc25
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-11-24 13:57:30 +0100

    forcing canvas renderer

--- pixi-html/photo.js
+++ pixi-html/photo.js
@@ -1,6 +1,6 @@
 import { Application } from 'pixi.js'
 
 export default (node, user) => {
-  const app = new Application(50, 50, {backgroundColor: 0x000000})
+  const app = new Application(50, 50, {backgroundColor: 0x000000, forceCanvas: true})
   node.appendChild(app.view)
 }

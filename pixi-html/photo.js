b7a89455aa512725f0ad0ddbbf5cde7fab9ce768
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-11-28 12:39:19 +0100

    first draft of loading

--- pixi-html/photo.js
+++ pixi-html/photo.js
@@ -1,10 +1,15 @@
-import { Sprite } from 'pixi.js'
+import { Sprite, loader } from 'pixi.js'
 
 export default ({picture, width, y = 0, x = 0}) => {
-  const photo = Sprite.fromImage(picture)
+  const photo = Sprite.fromImage('./assets/spinner.png')
   photo.width = width
   photo.height = width
   photo.y = y
   photo.x = x
+
+  loader.add(picture).load(() => {
+    console.log(loader.resources[picture].texture)
+  })
+
   return photo
 }

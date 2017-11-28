434fa32367ccecb9b4679aacc1c6df8058fe6f8b
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-11-28 22:48:44 +0100

    animating text

--- pixi-html/listitem.js
+++ pixi-html/listitem.js
@@ -9,9 +9,14 @@ const fontColor = style['primary-text-color']
 const fontFamily = parseFontType(style.font)
 const ROW_HEIGHT = stripPx(style['row-height'])
 const ROW_WIDTH = stripPx(style['row-width'])
+const ROW_FONT_SIZE = stripPx(style['row-font-size'])
+const ROW_MAX_FONT_SIZE = stripPx(style['row-max-font-size'])
+
+const MIN_TEXT_X = photoSize + padding * 2
 
 const createBox = () => {
   const box = new Graphics()
+  box.beginFill(0xFFFFFF)
   box.lineStyle(2, hexToInt(fontColor))
   box.drawRect(0, 0, ROW_WIDTH, ROW_HEIGHT)
   return box
@@ -20,12 +25,12 @@ const createBox = () => {
 const createText = name => {
   const style = new TextStyle({
     fontFamily,
-    fontSize: 18,
+    fontSize: ROW_FONT_SIZE,
     fill: fontColor
   })
 
   const textElement = new Text(name, style)
-  textElement.x = photoSize + padding * 2
+  textElement.x = MIN_TEXT_X
   textElement.y = (ROW_HEIGHT - textElement.height) / 2
 
   return textElement
@@ -55,6 +60,31 @@ const createRow = ({user, showSeparator, index, app, coords}) => {
   row.addChild(box)
   row.addChild(photo)
   row.addChild(text)
+
+  row.interactive = true
+  row.buttonMode = true
+
+  let shouldGrow = false
+
+  row.on('pointerover', () => {
+    shouldGrow = true
+  })
+
+  row.on('pointerout', () => {
+    shouldGrow = false
+  })
+
+  app.ticker.add(delta => {
+    if (shouldGrow) {
+      text.style.fontSize = Math.min(ROW_MAX_FONT_SIZE, text.style.fontSize + 0.5 * delta)
+      const maxTextX = ROW_WIDTH - padding - text.width
+      text.x = Math.min(maxTextX, text.x + 10 * delta)
+    } else {
+      text.style.fontSize = Math.max(ROW_FONT_SIZE, text.style.fontSize - 0.5 * delta)
+      text.x = Math.max(MIN_TEXT_X, text.x - 10 * delta)
+    }
+    text.y = (ROW_HEIGHT - text.height) / 2
+  })
 
   return row
 }
